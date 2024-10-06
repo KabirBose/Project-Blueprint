90588856e4e929d1ab6ab8c0fee71f9b2ad53b25
Kabir Bose <kabirbose04@gmail.com>
2024-10-06 18:16:07 -0400

    changed text on landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,14 @@ import Image from "next/image";
 export default function Page() {
   return (
     <section className="bg-blue-600 text-white h-[100vh] flex flex-row-reverse justify-center items-center">
-      <div>
-        <h1 className="font-bold text-4xl drop-shadow-xl">Project Blueprint</h1>
-        <p className="text-xl drop-shadow-xl">
+      <div className="w-1/2">
+        <h1 className="font-bold text-3xl drop-shadow-xl">
           The modern schedule builder for colleges and universities.
+        </h1>
+        <p className="text-xl drop-shadow-xl">
+          Say goodbye to outdated scheduling systems and make sure you get your
+          courses on time. Developed with a fast and simple drag and drop
+          interface in mind.
         </p>
       </div>
       <Image
