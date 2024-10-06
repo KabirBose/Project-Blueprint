a3bc318f2d00c024cdd6a92f82bb390433d2081f
Kabir Bose <kabirbose04@gmail.com>
2024-10-06 18:29:11 -0400

    updated landing page vector

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,21 +2,21 @@ import Image from "next/image";
 
 export default function Page() {
   return (
-    <section className="h-[100vh] flex flex-row-reverse justify-center items-center">
+    <section className="h-[100vh] flex flex-row-reverse justify-center items-center gap-16">
       <div className="w-1/2">
         <h1 className="font-bold text-3xl drop-shadow-xl">
-          The modern schedule builder for colleges and universities.
+          The modern schedule builder for colleges and universities!
         </h1>
+        <br />
         <p className="text-xl drop-shadow-xl">
           Say goodbye to outdated scheduling systems and make sure you get your
-          courses on time. Developed with a fast and simple drag and drop
+          courses on time. Developed with a fast and simple "drag and drop"
           interface in mind.
         </p>
       </div>
       <Image
-        className="drop-shadow-xl"
-        src="/vectors/schedule.svg"
-        alt="University schedule"
+        src="/vectors/students.svg"
+        alt="University students studying"
         height={300}
         width={300}
       />
--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className="flex items-center justify-between gap-4 p-4">
+    <nav className="flex items-center justify-between gap-4 p-4 font-medium">
       <div className="flex justify-between items-center gap-4 w-full bg-white p-6 rounded-full shadow-xl">
         <div className="flex gap-4">
           <Link href={""}>Home</Link>
