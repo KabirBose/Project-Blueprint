457a8884c7d8eca2ac0dc6c3d4ad37b9e0af2a16
Kabir Bose <kabirbose04@gmail.com>
2024-10-06 17:10:05 -0400

    added vector, heading, subheading to landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,21 @@
+import Image from "next/image";
+
 export default function Page() {
   return (
-    <div>
-      <h1 className="font-bold text-3xl">Project Blueprint</h1>
-      <p>The modern schedule builder for colleges and universities.</p>
-    </div>
+    <section className="bg-blue-600 text-white h-[100vh] flex flex-row-reverse justify-center items-center">
+      <div>
+        <h1 className="font-bold text-4xl drop-shadow-xl">Project Blueprint</h1>
+        <p className="text-xl drop-shadow-xl">
+          The modern schedule builder for colleges and universities.
+        </p>
+      </div>
+      <Image
+        className="drop-shadow-xl"
+        src="/vectors/schedule.svg"
+        alt="University schedule"
+        height={300}
+        width={300}
+      />
+    </section>
   );
 }
